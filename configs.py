25de77597bd7e8c886d86fb77b737238bce821cf
WHITE DEVIL - SM <105021702+WhiteDevilSM@users.noreply.github.com>
2023-01-30 00:12:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,18 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 11508650))
+    API_HASH = os.environ.get("API_HASH", "d4053a01b1c02f705c45a1e30496d11e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6199157936:AAG6cemg4C_bW4sDNEe8nCU5jxTUQX91x6I")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001628019485))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "SM_link_search_bot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1661411006))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
-🤖 My Name: <a href='https://t.me/cyniteofficial'>Mdisk Search Robot</a>
+🤖 My Name: <a href='https://t.me/SM_Updates_1'Link Search Robot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -23,7 +23,7 @@ class Config(object):
 
 📡 Server: <a href='https://heroku.com'>Heroku</a>
 
-👨‍💻 Created By: <a href='https://t.me/cyniteofficial'>Cynite</a></b>
+👨‍💻 Created By: <a href='https://t.me/White_devil_123'>Cynite</a></b>
 """
 
     ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/cyniteofficial'>Click Me</a>
@@ -34,22 +34,22 @@ If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
     HOME_TEXT = """
 <b>Hey! {}😅,
 
-I'm Mdisk Search Robot.🤖</a>
+I'm Link Search Robot.🤖</a>
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @Cyniteofficial</a></b>
+<a>Made With ❤ By @White_devil_123</a></b>
 """
 
 
     START_MSG = """
 <b>Hey! {}😅,
 
-I'm Mdisk Search Robot.🤖</a>
+I'm Link Search Robot.🤖</a>
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @Cyniteofficial</a></b>
+<a>Made With ❤ By @White_devil_123</a></b>
 """
 
 
